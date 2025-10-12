ddd0c44631970970ac77ba00d0a74d26f033e169
Samuel <smenxah@gmail.com>
2025-10-12 19:29:15 +0000

    Add main function and account type prompts to BankAccount.kt

---
+++ BankAccount.kt
@@ -0,0 +1,15 @@
+// File: BankAccount.kt
+
+/**
+ * You can edit, run, and share this code.
+ * play.kotlinlang.org
+ */
+fun main() {
+    println("Welcome to your banking system.")
+    
+    println("What type of account would you like to create?")
+	println("")
+	println("1. Debit account")
+	println("2. Credit account")
+	println("3. Checking account")
+}
